16f15de19ff78f9f1cba02465b384a511fedcb8e
Julian zapata rugeles <juliang.@@>
2021-01-09 11:13:17 -0500

    se añadio eventos del teclado y el personaje se mueve en la escena

--- main.cpp
+++ main.cpp
@@ -2,20 +2,22 @@
 #include <QGraphicsItem>
 #include <QGraphicsScene>
 #include <QGraphicsView>
-#include <personaje.h>
+#include <personaje.h> // propia
 #include <QKeyEvent>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     // creación de la escena //
-    QGraphicsScene * scene  = new QGraphicsScene(0,0,1000,500);
+    QGraphicsScene * scene  = new QGraphicsScene();
     // Item para escena //
-    personaje * personajePrincipal = new personaje(100,100,20,20);
+    personaje * personajePrincipal = new personaje(0,100,20,20);
 
     // anexo a escena //
     scene->addItem(personajePrincipal);
     // ejecutar ventana //
+
     QGraphicsView * vista = new QGraphicsView(scene);
     vista->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     vista->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
--- personaje.cpp
+++ personaje.cpp
@@ -2,8 +2,19 @@
 
 personaje::personaje(short int x , short int y , short int ancho , short int alto){
     this->setRect(x,y,ancho,alto);
+    this->setFlag(QGraphicsItem::ItemIsFocusable);
+    this->setFocus();
 }
 
 void personaje::keyPressEvent(QKeyEvent *event)
 {
+    if(event->key() == Qt::Key_A){
+        setPos(x()-5,y());
+    }
+    if(event->key() == Qt::Key_D){
+        setPos(x()+5,y());
+    }
+    if(event->key() == Qt::Key_W){
+        setPos(x(),y()-5);
+    }
 }
